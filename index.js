78062c9c27bc228ed82a14da5e872ccc9f56f282
Srikanth Murarishetti <m.srikanthsri77@gmail.com>
2021-06-27 18:34:41 +0530

    Javascript Introduction

---
+++ index.js
@@ -0,0 +1,59 @@
+console.log("Hey from file")
+let name="sri";
+let age=100;
+
+console.log(name);
+console.log(age);
+
+ name="srikanth";
+ age=200;
+
+console.log(name);
+console.log(age);
+
+
+//array
+
+var num=[1,2,3,4,5,6]
+console.log(num[1])
+
+//object
+
+var obj={
+    name:"srikanth",
+    id:123,
+    section:"c"
+}
+
+console.log(obj)
+console.log(obj.name)
+
+//obj with array
+
+var obj1=[
+    {
+    name:"srikanth",
+    id:123,
+    section:"c"
+    },
+    {
+        name:"prashanth",
+        id:167,
+        section:"c"
+        },
+        {
+        name:"kranthi",
+        id:1221,
+        section:"c"
+        },
+]
+
+console.log(obj1)
+console.log(obj[1])
+
+
+var a=2;
+var b="2";
+
+a===b ? console.log("true"):console.log("false");
+a==b ? console.log("true"):console.log("false");
